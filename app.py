4fde3525e36ef134d93193a5e11515ff52d2ce69
gaurav <imgauravchandani@gmail.com>
2022-02-13 13:17:09 +0530

    added: book other details

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ indices = pickle.load(open('book_indices_new.pkl','rb'))
 splitted_arrs_0 = pickle.load(open('book_similarity_new1.pkl','rb'))
 splitted_arrs_1 = pickle.load(open('book_similarity_new2.pkl','rb'))
 splitted_arrs_2 = pickle.load(open('book_similarity_new3.pkl','rb'))
-book_img = pickle.load(open('book_images_new.pkl','rb'))
+book_img = pickle.load(open('book_other_details_new.pkl','rb'))
 titles = pickle.load(open('book_title_array_new.pkl','rb'))
 
 cosine_sim_corpus = np.concatenate((splitted_arrs_0, splitted_arrs_1, splitted_arrs_2))
@@ -35,11 +35,18 @@ def predict():
     for i in book_indices:
         recommended_books.append(titles[i])
     images = []
+    authors = []
+    publication_year = []
     for item in recommended_books:
         images.append(book_img["image_url"].loc[item])
+        authors.append(book_img["authors"].loc[item])
+        publication_year.append(book_img["original_publication_year"].loc[item])
 
     result={'title':recommended_books,
-       'images':images}
+       'images':images,
+       'authors': authors,
+       'publication_year': publication_year
+       }
 
     resultjson = json.dumps(result)
 
